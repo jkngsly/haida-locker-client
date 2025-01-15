d4890f3590cd55f94916e3443ed7ab90cc982e38
Jordan Kingsley <jkngsly@gmail.com>
2025-01-14 21:37:11 -0500

    Bug

--- src/features/FolderTree/FolderTree.tsx
+++ src/features/FolderTree/FolderTree.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { useAppDispatch } from "../../app/hooks"
 import { setId } from './folderTreeSlice';
 import { useGetRootFoldersQuery } from './folderTreeApiSlice'
-import TreeChild from "./FolderTreeChild"
+import FolderTreeChild from "./FolderTreeChild"
 
 const FolderTree: React.FC = () => {
     const dispatch = useAppDispatch()
@@ -20,7 +20,7 @@ const FolderTree: React.FC = () => {
                 <li><a onClick={handleCLick}>Home</a></li>
                 {data.children && data.children.map(function (c) {
                     return (
-                        <TreeChild key={c.id + "_parent"} child={{
+                        <FolderTreeChild key={c.id + "_parent"} child={{
                             id: c.id,
                             name: c.name,
                             children: c.children
