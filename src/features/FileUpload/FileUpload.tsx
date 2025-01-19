2bb06665c1e493950e93a451d9ddba866b0dccfe
Jordan Kingsley <jkngsly@gmail.com>
2025-01-19 18:55:01 -0500

    Update FileUpload.tsx

--- src/features/FileUpload/FileUpload.tsx
+++ src/features/FileUpload/FileUpload.tsx
@@ -3,25 +3,56 @@ import React from 'react'
 import Button from '@components/Button';
 
 interface Props {
+
 }
 
-const FileUploadList: React.FC<Props> = (props) => {
+interface UploadFile { 
+    name: string,
+    size: number,
+    type: string,
+    url: string
+}
+
+const fileUpload: React.FC<Props> = (props) => {
     const fileInputRef = React.useRef<HTMLInputElement>(null);
+
+    const handleUploadClick = (): void => {
+        fileInputRef.current?.click();
+    }
+
+    const handleUploadChange = (e: React.ChangeEvent<HTMLInputElement>): void => { 
+        if (e.target.files) {
+
+            //setFolderTreeLocked(true);
+      
+            const files: UploadFile[] = [];
+            [...e.target.files].map((file: File) => { 
+              // FolderView icons
+              files.push({
+                name: file.name,
+                size: file.size,
+                type: file.type,
+                url: URL.createObjectURL(file)
+              });
+            });
+        }
+    }
+
 
     return (
         <>
         
-        <input
-            type="file"
-            //ref={fileInputRef}
-            multiple
-            style={{ display: 'none' }}
-            //onChange={}
-            id="upload" />
-            <Button text="Upload File" heroIcon="CloudArrowUp" />
+            <input
+                type="file"
+                ref={fileInputRef}
+                multiple
+                style={{ display: 'none' }}
+                onChange={handleUploadChange}
+                id="upload" />
+            <Button text="Upload File" onClick={handleUploadClick} heroIcon="CloudArrowUp" />
             <Button text="Create Folder" heroIcon="FolderPlus" />
         </>
     )
 };
 
-export default FileUploadList;
+export default fileUpload;
