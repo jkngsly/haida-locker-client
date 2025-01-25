7cd61aee82af39b67f75304405735c4909a3244e
Jordan Kingsley <jkngsly@gmail.com>
2025-01-25 16:56:08 -0500

    CSS

--- src/features/settings/Settings.tsx
+++ src/features/settings/Settings.tsx
@@ -5,9 +5,26 @@ const Settings: React.FC = () => {
     return(
         <>
             <div id="settings" className="box">
-                Admin Settings
-
-            
+                <span className="box-title">Administrator</span>
+                <div className="settings-box">
+                    <div></div>
+                </div>
+                <span className="box-title">Profile Settings</span>
+                <div className="settings-box">
+                    <div></div>
+                </div>
+                <span className="box-title">Site Settings</span>
+                <div className="settings-box">
+                    <div></div>
+                </div>
+                <span className="box-title">Theme Configuration</span>
+                <div className="settings-box">
+                    <div></div>
+                </div>
+                <span className="box-title">Manage Users</span>
+                <div className="settings-box">
+                    <div></div>
+                </div>
             </div>
         </>
     )
