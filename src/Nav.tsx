be895cc48085f0c4e4c44462d805b5cf4c2832dc
Jordan Kingsley <jkngsly@gmail.com>
2025-01-28 18:59:10 -0500

    Nav to use hero icons

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -5,6 +5,7 @@ import Logo from '@images/haida.png'
 import '@sass/Nav.scss'
 import { setToken } from "@features/auth/authSlice"
 import { useAppDispatch } from '@/app/hooks'
+import HeroIcon from '@/components/HeroIcon'
 
 function Nav() {
     const [count, setCount] = React.useState(0)
@@ -15,18 +16,18 @@ function Nav() {
     const links = [
         {
             to: "portal/drive",
-            svgDraw: "M21.75 17.25v-.228a4.5 4.5 0 0 0-.12-1.03l-2.268-9.64a3.375 3.375 0 0 0-3.285-2.602H7.923a3.375 3.375 0 0 0-3.285 2.602l-2.268 9.64a4.5 4.5 0 0 0-.12 1.03v.228m19.5 0a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3m19.5 0a3 3 0 0 0-3-3H5.25a3 3 0 0 0-3 3m16.5 0h.008v.008h-.008v-.008Zm-3 0h.008v.008h-.008v-.008Z",
+            icon: "Server",
             text: "Drive",
             folders: true
         },
         {
             to: "portal",
-            svgDraw: "m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25",
+            icon: "Home",
             text: "Dashboard",
         },
         {
             to: "portal/settings",
-            svgDraw: "M6 13.5V3.75m0 9.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 3.75V16.5m12-3V3.75m0 9.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 3.75V16.5m-6-9V3.75m0 3.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 9.75V10.5",
+            icon: "AdjustmentsVertical",
             text: "Settings"
         },
     ];
@@ -56,9 +57,7 @@ function Nav() {
                         return (
                             <div className={link.to} key={index}>
                                 <NavLink to={link.to}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 mr-5">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d={link.svgDraw} />
-                                    </svg>
+                                    <HeroIcon name={link.icon} />
                                     {link.text}
                                 </NavLink>
                                 
