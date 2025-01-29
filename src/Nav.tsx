1e25c6b4aa8c05496636773e8f70dd900aa2d901
Jordan Kingsley <jkngsly@gmail.com>
2025-01-28 21:14:06 -0500

    UI redesign

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -17,13 +17,8 @@ function Nav() {
         {
             to: "portal/drive",
             icon: "Server",
-            text: "Drive",
+            text: "My Drive",
             folders: true
-        },
-        {
-            to: "portal",
-            icon: "Home",
-            text: "Dashboard",
         },
         {
             to: "portal/settings",
@@ -51,40 +46,38 @@ function Nav() {
 
     return (
         <div id="navigation">
-            <div>
-                <div id="logo">
-                    <img src={Logo} />Haida<span>ドライブ</span>
-                </div>
-                <div id="user" className="pl-5 pt-5 pb-5 flex">
-                    <span className="flex flex-row justify-center align-middle">
-                        <div className="rounded-full pfp w-12 h-12 border-2 border-slate-100"></div>
-                        <div className="ml-3 flex flex-col">
-                            <span>Kita Ruuki</span>
-                            <span className="text-sm">Online</span>
-                        </div>
-                    </span>
-                </div>
-                <nav className="flex flex-col w-full">
-                    {links.map((link, index) => {
-                        return (
-                            <div className={link.to} key={index}>
-                                <NavLink to={link.to}>
-                                    <HeroIcon name={link.icon} />
-                                    {link.text}
-                                </NavLink>
-
-                                {link.to == "portal/drive" && (
-                                    <FolderTree />
-                                )}
-
-                            </div>
-                        )
-                    })}
-                    <div onClick={handleLogoutClick}>
-                        Logout
-                    </div>
-                </nav>
+            <div id="logo">
+                {/*&Haida<span></span>*/}
+                Haida<span>ドライブ</span>
             </div>
+            <nav className="flex flex-col w-full">
+                {links.map((link, index) => {
+                    return (
+                        <div className={link.to} key={index}>
+                        <NavLink to={link.to}>
+                            <HeroIcon name={link.icon} />
+                            {link.text}
+                        </NavLink>
+
+                        {link.to == "portal/drive" && (
+                            <FolderTree />
+                        )}
+
+                    </div>
+
+                    )
+                })}
+            </nav>
+            {/*
+            <div id="user" className="pl-5 pt-5 pb-5 flex">
+                <span className="flex flex-row justify-center align-middle">
+                    <div className="rounded-full pfp w-12 h-12 border-2 border-slate-100"></div>
+                    <div className="ml-3 flex flex-col">
+                        <span>Kita Ruuki</span>
+                        <span className="text-sm">Online</span>
+                    </div>
+                </span>
+            </div> */}  
         </div>
     )
 }
